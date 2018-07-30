c57281c44e5b0ef4a52cfe276038bd183ab9aa38
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2018-07-30 16:04:39 -0400

    Jul-30-2018_4:04_PM

---
+++ Fresnel/App.xaml.cs
@@ -0,0 +1,39 @@
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Fresnel
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            AppCenter.Start("ios=00611398-2571-4e1f-9355-2e0a8260304e;" +
+                            "uwp={Your UWP App secret here};" +
+                            "android=ea305699-1808-4626-bba7-2d654ed7028e;",
+                            typeof(Analytics), typeof(Crashes));
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Fresnel/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Fresnel.Views;
+using Fresnel.MenuItems;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Fresnel
+{
+
+    public partial class MainPage : MasterDetailPage
+    {
+
+        public List<MasterPageItem> MenuList { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            MenuList = new List<MasterPageItem>();
+
+            // Creating our pages for menu navigation
+            // Here you can define title for item, 
+            // icon on the left side, and page that you want to open after selection
+            var page1 = new MasterPageItem() { Title = "Home - CSS System Status", Icon = "itemIcon1.png", TargetType = typeof(Page1) };
+            var page2 = new MasterPageItem() { Title = "Total Open Incidents", Icon = "itemIcon2.png", TargetType = typeof(Page2) };
+            var page3 = new MasterPageItem() { Title = "Operations", Icon = "itemIcon3.png", TargetType = typeof(Page3) };
+            var page4 = new MasterPageItem() { Title = "SPDR Heat Map", Icon = "itemIcon4.png", TargetType = typeof(Page4) };
+            var page5 = new MasterPageItem() { Title = "Email Statistics", Icon = "itemIcon5.png", TargetType = typeof(Page5) };
+            var page6 = new MasterPageItem() { Title = "Current Foreign Travel", Icon = "itemIcon6.png", TargetType = typeof(Page6) };
+            var page7 = new MasterPageItem() { Title = "Azure Data", Icon = "itemIcon7.png", TargetType = typeof(Page7) };
+            var page8 = new MasterPageItem() { Title = "Link to Source Documenta", Icon = "itemIcon8.png", TargetType = typeof(Page8) };
+
+
+            // Adding menu items to menuList. (!menu, menu!)
+            MenuList.Add(page1);
+            MenuList.Add(page2);
+            MenuList.Add(page3);
+            MenuList.Add(page4);
+            MenuList.Add(page5);
+            MenuList.Add(page6);
+            MenuList.Add(page7);
+            MenuList.Add(page8);
+
+            // Setting our list to be ItemSource for ListView in MainPage.xaml
+            navigationDrawerList.ItemsSource = MenuList;
+
+            // Initial navigation, this can be used for our home page
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(Page1)));
+        }
+
+        // Event for Menu Item selection, here we are going to handle navigation based
+        // on user selection in menu ListView
+        private void OnMenuItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            var item = (MasterPageItem)e.SelectedItem;
+            Type page = item.TargetType;
+
+            Detail = new NavigationPage((Page)Activator.CreateInstance(page));
+            IsPresented = false;
+        }
+    }
+}
